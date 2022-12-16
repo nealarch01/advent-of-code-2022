2b46c12edd70a00de768bcb38ad0e580f8deb6ac
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-12-15 17:47:59 -0800

    Fix comment

--- Dec04/CampCleanup.swift
+++ Dec04/CampCleanup.swift
@@ -12,8 +12,8 @@ func findOverlap(pair: [String]) -> Bool {
     let secondSection = getValues(section: pair[1])
     // Condition for overlap:
     // If Section A starting number is greater than or equal to the starting number of Section B
-    // If Section A starting number is less than or equal to the ending number of Section B
-    // Second condition is the same but reversed (Section B starting number)
+    // If Section A ending number is less than or equal to the ending number of Section B
+    // Second condition is the same but reversed (Section B)
     let condition1 =  secondSection.start >= firstSection.start && firstSection.end >= secondSection.end
     let condition2 = firstSection.start >= secondSection.start && secondSection.end >= firstSection.end
     return condition1 || condition2 // Return true if either condition evaluates to true
