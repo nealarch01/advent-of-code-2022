8ac74c99e048476e937f388be1e7ac807be82c0f
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-12-17 21:56:55 -0800

    Complete part 2 Dec06

--- Dec06/TuningTrouble.swift
+++ Dec06/TuningTrouble.swift
@@ -1,8 +1,8 @@
 import Foundation
 import Darwin 
 
-func isUnique(charMap: inout [Character:Int], fourArray: [Character]) -> Bool {
-    for char in fourArray {
+func isUnique(charMap: inout [Character:Int], charArray: [Character]) -> Bool {
+    for char in charArray {
         if charMap[char] != nil {
             return false
         }
@@ -17,6 +17,68 @@ func charArrayToString(charArray: [Character]) -> String {
         string.append(char)
     }
     return string
+}
+
+func messageMarker4(dataStream: String) -> Int {
+    var fourArray: [Character] = []
+
+    for i in 0..<4{
+        let char = dataStream[dataStream.index(dataStream.startIndex, offsetBy: i)]
+        fourArray.append(char)
+    }
+
+    var charMap: [Character:Int] = [:]
+
+    if isUnique(charMap: &charMap, charArray: fourArray) {
+        print("Unique: \(fourArray)")
+        exit(0)
+    }
+
+    // Since we already have the first four, start at index 4
+    for i in 4..<dataStream.count {
+        let char = dataStream[dataStream.index(dataStream.startIndex, offsetBy: i)]
+        charMap[fourArray[0]] = nil
+        fourArray.removeFirst()
+        fourArray.append(char)
+        var charMap: [Character:Int] = [:]
+        if isUnique(charMap: &charMap, charArray: fourArray) {
+            print("Marker: \(charArrayToString(charArray: fourArray))")
+            print(i + 1)
+            return i + 1
+        }
+    }
+    return 0
+}
+
+func messageMarker14(dataStream: String) -> Int {
+    var fourteenArray: [Character] = []
+
+    for i in 0..<14{
+        let char = dataStream[dataStream.index(dataStream.startIndex, offsetBy: i)]
+        fourteenArray.append(char)
+    }
+
+    var charMap: [Character:Int] = [:]
+
+    if isUnique(charMap: &charMap, charArray: fourteenArray) {
+        print("Unique: \(fourteenArray)")
+        exit(0)
+    }
+
+    // Since we already have the first fourteen, start at index 14
+    for i in 14..<dataStream.count {
+        let char = dataStream[dataStream.index(dataStream.startIndex, offsetBy: i)]
+        charMap[fourteenArray[0]] = nil
+        fourteenArray.removeFirst()
+        fourteenArray.append(char)
+        var charMap: [Character:Int] = [:]
+        if isUnique(charMap: &charMap, charArray: fourteenArray) {
+            print("Marker: \(charArrayToString(charArray: fourteenArray))")
+            print(i + 1)
+            return i + 1
+        }
+    }
+    return 0
 }
 
 func main() {
@@ -35,33 +97,8 @@ func main() {
     }
     
     // Code here
-    var fourArray: [Character] = []
-
-    for i in 0..<4{
-        let char = contents[contents.index(contents.startIndex, offsetBy: i)]
-        fourArray.append(char)
-    }
-
-    var charMap: [Character:Int] = [:]
-
-    if isUnique(charMap: &charMap, fourArray: fourArray) {
-        print("Unique: \(fourArray)")
-        exit(0)
-    }
-
-    // Since we already have the first four, start at index 4
-    for i in 4..<contents.count {
-        let char = contents[contents.index(contents.startIndex, offsetBy: i)]
-        charMap[fourArray[0]] = nil
-        fourArray.removeFirst()
-        fourArray.append(char)
-        var charMap: [Character:Int] = [:]
-        if isUnique(charMap: &charMap, fourArray: fourArray) {
-            print("Marker: \(charArrayToString(charArray: fourArray))")
-            print(i + 1)
-            exit(0)
-        }
-    }
+    var _ = messageMarker4(dataStream: contents)
+    var _ = messageMarker14(dataStream: contents)
 }
 
 main()
